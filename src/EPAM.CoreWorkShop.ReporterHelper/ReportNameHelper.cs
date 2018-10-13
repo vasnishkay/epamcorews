b26d040e6d260ee55f2d1a1b6fb20f351c062b9a
Vasiliy Buldakov <vasiliy_buldakov@epam.com>
2018-10-13 12:35:39 +0400

    HOL1. First version code and test

---
+++ src/EPAM.CoreWorkShop.ReporterHelper/ReportNameHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace EPAM.Core.ReporterHelper
+{
+    public static class ReportNameHelper
+    {
+        public static string NormalizeFileName(string name, char replChar)
+        {
+            var invalidChars = Path.GetInvalidFileNameChars();
+            foreach (var c in invalidChars)
+            {
+                name = name.Replace(c, replChar);
+            }
+
+            return name;
+        }
+    }
+}
