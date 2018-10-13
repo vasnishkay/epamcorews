3d639c790f51780529ac93dc215e0363e972b5da
Vasiliy Buldakov <vasiliy_buldakov@epam.com>
2018-10-13 13:11:46 +0400

    HOL1. Add pause to test start up

--- src/EPAM.CoreWorkShop.ReporterHelper.Tests/ReportNameHelperTests.cs
+++ src/EPAM.CoreWorkShop.ReporterHelper.Tests/ReportNameHelperTests.cs
@@ -1,11 +1,18 @@
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
 
 namespace EPAM.Core.ReporterHelper.Tests
 {
     [TestClass]
     public class ReportNameHelperTests
     {
+        [AssemblyInitialize]
+        public static void Init(TestContext context)
+        {
+            Thread.Sleep(30 * 1000);
+        }
+
         [TestMethod]
         [DataRow("Hello: my world", '_', "Hello_ my world")]
         [DataRow("Hello my %world", '_', "Hello my %world")]
