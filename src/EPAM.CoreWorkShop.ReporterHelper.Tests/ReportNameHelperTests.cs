6d32320c2646eb0ef7b0b74917ccca41a3855dd7
Vasiliy Buldakov <vasiliy_buldakov@epam.com>
2018-10-13 13:18:29 +0400

    HOL1. Add pause to test start up

--- src/EPAM.CoreWorkShop.ReporterHelper.Tests/ReportNameHelperTests.cs
+++ src/EPAM.CoreWorkShop.ReporterHelper.Tests/ReportNameHelperTests.cs
@@ -10,11 +10,11 @@ namespace EPAM.Core.ReporterHelper.Tests
         [AssemblyInitialize]
         public static void Init(TestContext context)
         {
-            Thread.Sleep(30 * 1000);
+            //Thread.Sleep(30 * 1000);
         }
 
         [TestMethod]
-        [DataRow("Hello: my world", '_', "Hello_ my world")]
+        //[DataRow("Hello: my world", '_', "Hello_ my world")]
         [DataRow("Hello my %world", '_', "Hello my %world")]
         [DataRow("Hello my /world", '_', "Hello my _world")]
         public void NormalizeFileNameTest(string name, char repl, string expected)
